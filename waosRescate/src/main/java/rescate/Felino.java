0f4799f0ecc2385ea0a93982879c5d3c60ba6372
Esteban Rivera <danielestebanriveragomez03@gmail.com>
2023-09-30 15:47:47 -0500

    clases creadas

---
+++ waosRescate/src/main/java/rescate/Felino.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rescate;
+
+/**
+ *
+ * @author USER
+ */
+public class Felino extends Animal {
+    boolean toxo;
+
+    public Felino(boolean toxo, String nombre, String raza, String color, int edad) {
+        super(nombre, raza, color, edad);
+        this.toxo = toxo;
+    }
+
+    public boolean isToxo() {
+        return toxo;
+    }
+
+    public void setToxo(boolean toxo) {
+        this.toxo = toxo;
+    }
+}
